cce4b29472c8f0a99600e731749f3946216579e0
jwoods1 <jwoods1@fullsail.edu>
2015-11-22 21:23:23 -0700

    added auth controller with restriction policies

--- config/policies.js
+++ config/policies.js
@@ -39,7 +39,11 @@ module.exports.policies = {
 		// Apply the `false` policy as the default for all of RabbitController's actions
 		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
 		// '*': false,
-
+'*': ['passport', 'sessionAuth'],
+ 
+ 'auth': {
+    '*': ['passport']
+  }
 		// For the action `nurture`, apply the 'isRabbitMother' policy
 		// (this overrides `false` above)
 		// nurture	: 'isRabbitMother',
